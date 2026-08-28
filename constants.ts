69e95d296099c3425f0beacbfd774a8cc9daf87e
EdneyOliver <edney.and@gmail.com>
2026-08-28 10:39:14 -0300

    feat(packages): update Essencial package image URL

--- constants.ts
+++ constants.ts
@@ -33,7 +33,7 @@ export const DJ_PACKAGES = [
     name: "Essencial",
     description: "Organização, música de qualidade e tranquilidade para eventos intimistas. Ideal para aniversários em salões de condomínio e reuniões familiares.",
     // Imagem do setup Essencial
-    imageUrl: "https://i.postimg.cc/NjKWXkbY/Essencial.png", 
+    imageUrl: "https://i.postimg.cc/rpk3pgfM/ESSENCIAL-01.jpg", 
     price: "R$ 950",
     features: [
       "DJ Edney com Playlist Personalizada, Curadoria Musical, Leitura de Pista e Animação",
